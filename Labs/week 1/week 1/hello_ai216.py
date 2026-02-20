b25415e9a6deae73e83f264f13ded6741114c1d9
danishfareed335 <danishfareed988@gmail.com>
2026-02-20 14:30:20 +0500

    Update and rename hello_ai216.py.txt to hello_ai216.py

--- Labs/week 1/week 1/hello_ai216.py
+++ Labs/week 1/week 1/hello_ai216.py
@@ -14,4 +14,5 @@ print(sys.version)
 print("\nWhat Programming for AI means to me:")
 print("Programming for AI helps me build intelligent systems.")
 print("It allows me to solve real-world problems using data.")
+
 print("It prepares me for future AI development.")
